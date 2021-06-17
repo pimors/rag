2a33de359f27e0417acb6a1a9f4e723141178804
pimors <idc@nightmare.life>
2021-06-17 05:23:12 +0200

    more testing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use ndarray::prelude::*;
 
-// currently only squares 1d arrays
-fn square(p_x: &Array1<f32>) -> Array1<f32> {
-  return p_x.mapv(|p_x: f32| p_x.powi(2));
-}
-
-//fn maximum(p_x: &Array1<f32>, p_y: &Array1<f32>) -> Array1<f32> {
-//  for i in p_x.iter() {
-//    print!("i-1: {}\n", p_y[*i as i32])
-//  }
-//
-//  return p_x * p_y;
-//}
-
 fn main() {
+  // test closure to get ndim
+  let getdim = |p_arr: &Array<f32, _>| {
+    p_arr.ndim()
+  };
+
+  // return squared input array
+  // can't handle differnt array ndims.
+  // all arrays must have same ndim during execution
+  // needs change                                                      I think
+  let square = |p_arr: &Array<f32, _>| {
+    p_arr.mapv(|p_arr: f32| p_arr.powi(2))
+  };
+
+  //let maximum = |p_arr: &Array<f32, _>| {
+  //  // needs to be implemented
+  //};
+
+  //let leakyRelu = |p_arr: &Array<f32, _>| {
+  //  // needs to be implemented
+  //};
+
   // testing some stuff
-  let a = array![
-    [1.,2.,3.],
-    [4.,5.,6.],
-  ];
 
   let x = array![1.,2.,3.];
   let y = array![4.,5.,6.];
@@ -31,10 +35,12 @@ fn main() {
   println!("X dot Y: {}", &x.dot(&y));
   
   // test square
-  println!("square x: {}", square(&x));
-  //println!("square x: {}", square(x));
+  //println!("square x: {}", square(&x));
   // test max
 //  println!("maximum x y: {}", maximum(&x,&y));
+  // test getdim
+  println!("getdim x: {}", getdim(&x));
+
 
   println!{"\n"};
 
@@ -58,6 +64,14 @@ fn main() {
   
   println!{"\n"};
 
+  let a = array![
+    [1.,2.,3.],
+    [4.,5.,6.],
+  ];
+
+  // test square
+  println!("square a: {}", square(&a));
+ 
   assert_eq!(a.ndim(), 2);
   assert_eq!(a.len(), 6);
   assert_eq!(a.shape(), [2, 3]);
